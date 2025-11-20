7f0372d2820b7c6affc5906a0b330c92c8e827e9
Vigneshviky2511 <vm2120@srmist.edu.in>
2025-11-20 16:21:49 +0530

    Basic Init

---
+++ app/(tabs)/help.tsx
@@ -0,0 +1,11 @@
+// filepath: c:\Users\Admin\Desktop\my-app\app\(tabs)\help.tsx
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function HelpScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Help Screen</Text>
+    </View>
+  );
+}
